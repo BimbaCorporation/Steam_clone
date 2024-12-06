2a9773767ae97e2951797dda6213485bb73ff665
andriyko31 <andrii.hladunenko@oa.edu.ua>
2024-12-06 12:50:09 +0200

    update mosiichuk work

--- steamproject/src/features/Browse.jsx
+++ steamproject/src/features/Browse.jsx
@@ -206,36 +206,45 @@ const Browse = () => {
       ) : (
         <div className="card-grid">
           {filteredDeals.map((deal) => (
-            <div className="card" key={deal.dealID}>
-              <div className="card-top">
-                <img
-                  src={deal.thumb}
-                  alt={deal.title}
-                  className="card-image"
-                />
-                <h3 className="card-title">{deal.title}</h3>
-              </div>
-              <div className="card-bottom">
-                <p>
-                  Магазин: <span>{deal.storeID}</span>
-                </p>
-                <p>
-                  Знижка: <span>{Math.round(deal.savings)}%</span>
-                </p>
-                <p>
-                  Ціна: <span>${deal.salePrice}</span>
-                </p>
-                <p>
-                  Рейтинг:{" "}
-                  <span>
-                    {deal.steamRatingText
-                      ? `${deal.steamRatingText} (${deal.steamRatingPercent}%)`
-                      : "N/A"}
-                  </span>
-                </p>
-              </div>
-            </div>
-          ))}
+  <a
+    href={`https://www.cheapshark.com/redirect?dealID=${deal.dealID}`}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="card-link"
+    key={deal.dealID}
+  >
+    <div className="card">
+      <div className="card-top">
+        <img
+          src={deal.thumb}
+          alt={deal.title}
+          className="card-image"
+        />
+        <h3 className="card-title">{deal.title}</h3>
+      </div>
+      <div className="card-bottom">
+        <p>
+          Магазин: <span>{deal.storeID}</span>
+        </p>
+        <p>
+          Знижка: <span>{Math.round(deal.savings)}%</span>
+        </p>
+        <p>
+          Ціна: <span>${deal.salePrice}</span>
+        </p>
+        <p>
+          Рейтинг:{" "}
+          <span>
+            {deal.steamRatingText
+              ? `${deal.steamRatingText} (${deal.steamRatingPercent}%)`
+              : "N/A"}
+          </span>
+        </p>
+      </div>
+    </div>
+  </a>
+))}
+
         </div>
       )}
     </div>
